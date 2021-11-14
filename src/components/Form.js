a6ed93a24f6c979d2eef7aa9e8cb9a091ae1ea54
WalterCosta <156-201707140059@users.noreply.gitlab.lasse.ufpa.br>
2021-11-14 16:47:59 -0300

    Feito Requisito 3 e correção do 2

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,9 @@ import PropTypes from 'prop-types';
 class Form extends React.Component {
   render() {
     const { cardName, cardDescription, cardAttr1, cardAttr2, cardAttr3, cardImage,
-      cardRare, cardTrunfo, hasTrunfo, isSaveButtonDisabled,
+      cardRare, cardTrunfo,
+      // hasTrunfo,
+      isSaveButtonDisabled,
       onInputChange, onSaveButtonClick } = this.props;
 
     return (
@@ -130,7 +132,7 @@ Form.propTypes = {
   cardImage: PropTypes.string.isRequired,
   cardRare: PropTypes.string.isRequired,
   cardTrunfo: PropTypes.bool.isRequired,
-  hasTrunfo: PropTypes.bool.isRequired,
+  // hasTrunfo: PropTypes.bool.isRequired,
   isSaveButtonDisabled: PropTypes.bool.isRequired,
   onInputChange: PropTypes.func.isRequired,
   onSaveButtonClick: PropTypes.func.isRequired,
